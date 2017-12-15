2c87a8738fc2f00b817491b5f647fc2a1a435c52
Luke Sontrop <luke@bluesaltlabs.com>
2017-12-15 14:36:52 -0800

    initial commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+/*
+import (
+	"fmt",
+	"github.com"
+)
+*/
+
+// authentication
+
+// get files
+
+// upload files
+
+// delete files
+
+// server?
+
+// other stuff?
+
---
+++ readme.md
@@ -0,0 +1,5 @@
+> I don't know what I'm doing. 
+
+# Introduction
+
+This application is being developed to allow automatic backups from my 
